a213e703828dae2747c14be538b229b15a2e0e0f
FrancescoPepi <francescopepi16@gmail.com>
2023-11-08 21:11:00 +0100

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <script>
-// import HeaderComponent from "./components/Header.vue";
+import ProjectListComponent from "./components/ProjectListComponent.vue";
+import axios from "axios";
 
 export default {
   data() {
     return {
-      title: "titolo",
+      title: "APP",
+      projects: [],
+      api: {
+        baseUrl: "http://127.0.0.1:8000/api/",
+      },
     };
   },
 
-  // 	methods:{
-  // 		myMethods(){
-  // 			...
-  // 		},
-  // 	},
+  methods: {
+    fetchProjects(uri = this.api.baseUrl + "project") {
+      axios.get(uri).then((responce) => {
+        this.projects = responce.data.data;
+      });
+    },
+  },
 
-  // components: {
-  //	MyComponent,
-  // },
+  components: {
+    ProjectListComponent,
+  },
 
   // 	props:{
   // 		passaggioInfo: stringa,
   //  	 },
+
+  created() {
+    this.fetchProjects();
+  },
 };
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+  <ProjectListComponent :projects="projects" />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/ProjectListComponent.vue
@@ -0,0 +1,40 @@
+<script>
+// import HeaderComponent from "./components/Header.vue";
+
+export default {
+  data() {
+    return {
+      title: "Project List",
+    };
+  },
+
+  // 	methods:{
+  // 		myMethods(){
+  // 			...
+  // 		},
+  // 	},
+
+  // components: {
+  //	MyComponent,
+  // },
+
+  props: {
+    projects: Array,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h2>{{ title }}</h2>
+    <div class="row row-cols-3 g-4">
+      <div class="col" v-for="project in projects">
+        <div class="card">
+          <div class="card-body">{{ project.name }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
